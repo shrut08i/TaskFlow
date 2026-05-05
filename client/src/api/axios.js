b26dbca533b3351896e7eb940c482846acca1a02
Shruti Jha <shrutijha1628@gmail.com>
2026-05-05 13:35:07 +0530

    fix API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: "https://taskflow-backend-6mg7.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
