a47319b1a610ba0c77e6c0142292c848a2361eb6
Shruti Jha <shrutijha1628@gmail.com>
2026-05-05 14:27:09 +0530

    fix: append /api to axios baseURL to match backend routes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskflow-backend-6mg7.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://taskflow-backend-6mg7.onrender.com/api",
 });
 
 export default API;
