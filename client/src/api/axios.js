6e76834b22270d6b5396158f11d05a8e34d02a69
Shruti Jha <shrutijha1628@gmail.com>
2026-05-05 15:17:05 +0530

    fix: add axios interceptor to send auth token

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -7,4 +7,12 @@ const API = axios.create({
   baseURL: finalBaseURL,
 });
 
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
 export default API;
