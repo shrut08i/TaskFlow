8756a4e97df0a3a8226994f27c5cf57da9f427ab
Shruti Jha <shrutijha1628@gmail.com>
2026-05-05 13:57:30 +0530

    final env fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,27 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskflow-backend-6mg7.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default API;
