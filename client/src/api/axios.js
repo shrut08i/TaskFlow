73aa7771fbb3e5629ecbce17570fb0f148e8528a
Shruti Jha <shrutijha1628@gmail.com>
2026-05-05 14:09:11 +0530

    force api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://taskflow-backend-6mg7.onrender.com",
 });
 
 export default API;
